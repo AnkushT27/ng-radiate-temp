5c736161423be11266d1d60081c8ca8516ffdf31
AnkushT27 <ankushtiwari102@gmail.com>
2020-03-30 11:15:50 +0530

    Modal changes for leads

--- src/app/leads/leads.module.ts
+++ src/app/leads/leads.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { LeadsRoutingModule } from './leads-routing.module';
 import { LeadsComponent } from './leads/leads.component';
+import { ModalModule } from '../../../node_modules/ngx-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
-    LeadsRoutingModule
+    LeadsRoutingModule,
+    ModalModule.forRoot()
   ],
   declarations: [LeadsComponent]
 })
--- src/app/leads/leads/leads.component.ts
+++ src/app/leads/leads/leads.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,TemplateRef } from '@angular/core';
 import {SideMenuService} from '../../side-menu.service'
+import {BsModalService,BsModalRef} from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-leads',
   templateUrl: './leads.component.html',
   styleUrls: ['./leads.component.css']
 })
 export class LeadsComponent implements OnInit {
+  config = {
+    animated: true,
+    keyboard: false,
+    backdrop: true,
+    ignoreBackdropClick: false
+  };
+  modalRef: BsModalRef;
+ 
 
-  constructor(private sidemenuservice : SideMenuService) { 
+  constructor(private sidemenuservice : SideMenuService,private modalService : BsModalService) { 
     this.sidemenuservice.changeNav({'menu':true})
   }
 
   ngOnInit() {
   }
+
+  viewProject(template:TemplateRef<any>){
+    this.modalRef = this.modalService.show(template,this.config)
+   }
+ 
+   closeModal(){
+     this.modalRef.hide()
+   }
 
 }
